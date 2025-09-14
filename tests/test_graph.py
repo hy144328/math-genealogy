4a44ef5599a8949cab3c10f77e4aba85ece4f893
Hans Yu <hans.yu@outlook.de>
2025-09-14 16:06:12 +0200

    Unit tests.

---
+++ tests/test_graph.py
@@ -0,0 +1,36 @@
+import pytest
+
+import math_genealogy.graph
+
+@pytest.fixture
+def g() -> math_genealogy.graph.Stammbaum:
+    return math_genealogy.graph.Stammbaum(
+        math_genealogy.graph.StammbaumNode(0, "hans", 2020),
+    )
+
+def test_stammbaum(
+    g: math_genealogy.graph.Stammbaum,
+):
+    assert 0 in g
+
+    assert 1 not in g
+    g.add(
+        math_genealogy.graph.StammbaumNode(1, name="matthew", year=2001),
+    )
+    assert 1 in g
+
+    assert 2 not in g
+    g.add(
+        math_genealogy.graph.StammbaumNode(2, name="luca", year=2013),
+    )
+    assert 2 in g
+
+    assert g[0].name == "hans"
+    assert g[1].name == "matthew"
+    assert g[2].name == "luca"
+
+    g.add_ancestors(0, ancestors=[1, 2])
+
+    ancestors_0 = g.get_ancestors(0)
+    assert 1 in ancestors_0
+    assert 2 in ancestors_0
