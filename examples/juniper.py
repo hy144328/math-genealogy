0997c22d86394a69e89ce5cf192c072b0eaa708d
Hans Yu <hans.yu@outlook.de>
2025-09-15 12:40:20 +0200

    More DRY.

--- examples/juniper.py
+++ examples/juniper.py
@@ -15,18 +15,9 @@ import math_genealogy.viz
 math_genealogy_logger = logging.getLogger(math_genealogy.__name__)
 math_genealogy_logger.setLevel(logging.DEBUG)
 
+MAX_LEVEL = 3
 ROOT_ID = 149678
-
-loader = math_genealogy.load.WebLoader()
-parser = math_genealogy.parse.Parser()
-scraper = math_genealogy.scrape.Scraper(loader, parser)
-printer = math_genealogy.viz.DotPrinter()
-
-tree = math_genealogy.graph.Stammbaum()
-scraper.scrape(tree, ROOT_ID, max_level=3)
-scraper.prune(tree, max_level=3)
-
-for student_ct, (name_it, year_it) in enumerate([
+STUDENTS = [
     ("Simon Rees", 2009),
     ("Gary Chandler", 2010),
     ("Larry Li", 2011),
@@ -40,7 +31,18 @@ for student_ct, (name_it, year_it) in enumerate([
     ("Nicholas Jamieson", 2018),
     ("José Aguilar Perez", 2019),
     ("Jack Brewster", 2019),
-]):
+]
+
+loader = math_genealogy.load.WebLoader()
+parser = math_genealogy.parse.Parser()
+scraper = math_genealogy.scrape.Scraper(loader, parser)
+printer = math_genealogy.viz.DotPrinter()
+
+tree = math_genealogy.graph.Stammbaum()
+scraper.scrape(tree, ROOT_ID, max_level=MAX_LEVEL)
+scraper.prune(tree, max_level=MAX_LEVEL)
+
+for student_ct, (name_it, year_it) in enumerate(STUDENTS):
     tree.add(math_genealogy.graph.StammbaumNode(-student_ct, name=name_it, year=year_it))
     tree.add_ancestors(-student_ct, [ROOT_ID])
 
