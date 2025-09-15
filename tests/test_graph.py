317868e300b973a81af0b0eac3111b46770ba28c
Hans Yu <hans.yu@outlook.de>
2025-09-15 02:10:24 +0200

    Extend graph.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,40 +3,50 @@ import pytest
 import math_genealogy.graph
 
 @pytest.fixture
-def g() -> math_genealogy.graph.Stammbaum:
+def tree() -> math_genealogy.graph.Stammbaum:
     res = math_genealogy.graph.Stammbaum()
     res.add(math_genealogy.graph.StammbaumNode(0, "hans", 2020))
     return res
 
 def test_stammbaum(
-    g: math_genealogy.graph.Stammbaum,
+    tree: math_genealogy.graph.Stammbaum,
 ):
-    assert 0 in g
+    assert 0 in tree
 
-    assert 1 not in g
-    g.add(
+    assert 1 not in tree
+    tree.add(
         math_genealogy.graph.StammbaumNode(1, name="matthew", year=2001),
     )
-    assert 1 in g
+    assert 1 in tree
 
-    assert 2 not in g
-    g.add(
+    assert 2 not in tree
+    tree.add(
         math_genealogy.graph.StammbaumNode(2, name="luca", year=2013),
     )
-    assert 2 in g
+    assert 2 in tree
 
-    assert g[0].name == "hans"
-    assert g[1].name == "matthew"
-    assert g[2].name == "luca"
+    assert tree[0].name == "hans"
+    assert tree[1].name == "matthew"
+    assert tree[2].name == "luca"
 
-    g.add_ancestors(0, ancestors=[1, 2])
+    tree.add_ancestors(0, ancestors=[1, 2])
 
     with pytest.raises(ValueError):
-        g.add_ancestors(0, ancestors=[0])
+        tree.add_ancestors(0, ancestors=[0])
 
     with pytest.raises(KeyError):
-        g.add_ancestors(0, ancestors=[-1])
+        tree.add_ancestors(0, ancestors=[-1])
 
-    ancestors_0 = g.get_ancestors(0)
+    ancestors_0 = tree.get_ancestors(0)
     assert 1 in ancestors_0
     assert 2 in ancestors_0
+
+    descendants_0 = tree.get_descendants(1)
+    assert 0 in descendants_0
+    assert 2 not in descendants_0
+
+    tree.remove(2)
+
+    assert 0 in tree.nodes
+    assert 1 in tree.nodes
+    assert 2 not in tree.nodes
